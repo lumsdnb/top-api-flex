3c7be04ee8d76bcf70326cafaadbadb667763027
lums <jeremyondrey@gmail.com>
2021-08-27 01:15:57 +0200

    gooo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textInput = document.querySelector('[type=search]');
 const searchButton = document.querySelector('.search-button');
 const errorEl = document.createElement('h2');
 
-function fetchRandom() {
+async function fetchRandom() {
   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=cats`, {
     mode: 'cors',
   })
