e6782715e2fe7b8b8212fda96ac507638bf62707
lumsdnb <jeremy@lums.io>
2021-08-27 13:46:43 +0200

    Rewrote to use async await

--- script.js
+++ script.js
@@ -24,25 +24,15 @@ async function fetchRandom() {
     });
 }
 
-function fetchSpecific(input) {
-  fetch(
+async function fetchSpecific(input) {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${input}`,
     {
       mode: 'cors',
     }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch((error) => {
-      errorEl.textContent = error;
-      img.parentElement.prepend(errorEl);
-      img.src = '#';
-      img.alt = 'nothing found...';
-    });
+  );
+  const responseData = await response.json();
+  img.src = responseData.data.images.original.url;
 }
 
 randomButton.addEventListener('click', fetchRandom);
